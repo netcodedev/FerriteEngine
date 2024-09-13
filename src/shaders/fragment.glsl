3e22f1c68fce946f871fe94b46c3c60806fc7865
Benedict Mondini <benmondini@netcode.dev>
2024-09-13 12:36:58 +0200

    Beter normal calculation

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -8,16 +8,20 @@ out vec4 FragColor;
 void main()
 {
     vec3 unitNormal = normalize(Normal);
+    vec3 normal = vec3(0.0, 0.0, 0.0);
+
+    if (abs(unitNormal.y) > abs(unitNormal.x) && abs(unitNormal.y) > abs(unitNormal.z)) {
+        normal = vec3(0.0, 1.0, 0.0);
+    } else if (abs(unitNormal.x) > abs(unitNormal.y) && abs(unitNormal.x) > abs(unitNormal.z)) {
+        normal = vec3(1.0, 0.0, 0.0);
+    } else {
+        normal = vec3(0.0, 0.0, 1.0);
+    }
+    normal = normalize(normal);
+
     vec3 unitToLightVector = normalize(toLightVector);
-    float intensity = dot(unitNormal, unitToLightVector);
+    float intensity = dot(normal, unitToLightVector);
     float brightness = max(intensity, 0.5);
     vec3 diffuse = brightness * vec3(1.0);
-    // if(unitNormal.r > 0.0) {
-    //     FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    // } else if(unitNormal.g > 0.0) {
-    //     FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-    // } else if(unitNormal.b > 0.0) {
-    //     FragColor = vec4(0.0, 0.0, 1.0, 1.0);
-    // }
     FragColor = outColor * vec4(diffuse, 1.0);
 }
