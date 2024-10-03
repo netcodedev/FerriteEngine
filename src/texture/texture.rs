60a0053bcb0ced34601722108ea51b0243976729
netcodedev <benmondini@netcode.dev>
2024-10-03 12:47:12 +0200

    Changed to nearest filtering for crisp textures

--- src/texture/texture.rs
+++ src/texture/texture.rs
@@ -14,7 +14,7 @@ impl Texture {
         let img = image::open(path).expect("Bild konnte nicht geladen werden").flipv().to_rgba8();
         unsafe {
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
             gl::TexImage2D(
