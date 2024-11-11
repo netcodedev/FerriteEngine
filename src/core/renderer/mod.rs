2c8a050325270d443b23b1eb20eb687d33b73297
Benedict Mondini <benmondini@netcode.dev>
2024-11-11 18:21:40 +0100

    Removed unused stuff

--- src/core/renderer/mod.rs
+++ src/core/renderer/mod.rs
@@ -3,7 +3,6 @@ pub mod light;
 pub mod line;
 pub mod plane;
 pub mod shader;
-pub mod shadow;
 pub mod text;
 pub mod texture;
 pub mod ui;
--- src/core/renderer/shadow/fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-out vec4 FragColor;
-
-uniform sampler2D texture;
-
-void main() {
-    FragColor = vec4(1.0);
-}
