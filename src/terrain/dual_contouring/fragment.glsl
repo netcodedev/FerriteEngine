cdb82e42878a16c03b42e0a669d2fc0685763edf
Benedict Mondini <benmondini@netcode.dev>
2024-11-13 10:45:40 +0100

    Increased shadow smoothing, made shadow lighter

--- src/terrain/dual_contouring/fragment.glsl
+++ src/terrain/dual_contouring/fragment.glsl
@@ -20,13 +20,13 @@ float ShadowCalculation(vec4 fragPosLightSpace, vec3 toLightVector, vec3 normal)
     float bias = max(0.01 * (1.0 - dot(normal, toLightVector)), 0.005);
     float shadow = 0.0;
     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
-    for(int x = -1; x <= 1; ++x) {
-        for(int y = -1; y <= 1; ++y) {
+    for(int x = -2; x <= 2; ++x) {
+        for(int y = -2; y <= 2; ++y) {
             float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
             shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;        
         }    
     }
-    shadow /= 9.0;
+    shadow /= 25.0;
     return shadow;
 }
 
@@ -41,5 +41,5 @@ void main() {
     float brightness = max(intensity, 0.5);
     vec3 diffuse = brightness * vec3(1.0);
     float shadow = ShadowCalculation(fragPosLightSpace, unitToLightVector, normal);
-    FragColor = vec4((0.3 + (1.0 - shadow) * diffuse) * Color, 1.0);
+    FragColor = vec4((0.5 + (1.0 - shadow) * diffuse) * Color, 1.0);
 }
